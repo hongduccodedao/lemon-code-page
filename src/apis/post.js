68e637f9652a371f0225c3c93df62573b0df832c
hongduccodedao <contact.hongduc@gmail.com>
2023-07-30 16:17:13 +0700

    ✨ Get API with react-query and create post card

---
+++ src/apis/post.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const getPosts = async () => {
+  try {
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/posts`);
+    if (response.status === 200) {
+      return response.data;
+    }
+    return [];
+  } catch (error) {
+    console.log("🚀 ~ getProperties ~ error:", error);
+  }
+};
