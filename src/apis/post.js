1d2485976b282e4afda5cc29e307bad69a4c8297
hongduccodedao <contact.hongduc@gmail.com>
2023-07-31 21:37:25 +0700

    ✏️ Update

--- src/apis/post.js
+++ src/apis/post.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const getPosts = async () => {
+export const apiGetPosts = async () => {
   try {
     const response = await axios.get(
       `/api/posts`
@@ -14,16 +14,15 @@ export const getPosts = async () => {
   }
 };
 
-export const getPostBySlug = async (slug) => {
+export const apiGetPostBySlug = async (slug) => {
   try {
-    const response = await axios.get(
-      `/api/posts/${slug}`
-    );
+    const response = await axios.get(`/api/posts/${slug}`);
     if (response.status === 200) {
       return response.data;
     }
     return {};
   } catch (error) {
-    console.log("🚀 ~ getProperties ~ error:", error);
+    console.log("🚀 ~ apiGetPostBySlug ~ error:", error);
+    return {};
   }
 };
