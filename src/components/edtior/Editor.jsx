8a8a28aa7dc3ba422bf1f66b7ffa66c238d6efe0
Nguyễn Hồng Đức <contact.hongduc@gmail.com>
2023-08-06 16:31:09 +0700

    Update Editor.jsx

--- src/components/edtior/Editor.jsx
+++ src/components/edtior/Editor.jsx
@@ -2,11 +2,11 @@ import "@uiw/react-md-editor/markdown-editor.css";
 import "@uiw/react-markdown-preview/markdown.css";
 import dynamic from "next/dynamic";
 
-const MDEditor = dynamic(() => import("@uiw/react-md-editor"), { ssr: true });
+const MDEditor = dynamic(() => import("@uiw/react-md-editor"), { ssr: false });
 
 const Editor = ({ value, onChange }) => {
   return (
-    <div data-color-mode="dark">
+    <div>
       <MDEditor value={value} onChange={onChange} />
     </div>
   );
