74c529182bee9358be46aaaae731ff03adf91b10
hongduccodedao <contact.hongduc@gmail.com>
2023-08-06 22:26:34 +0700

    ✨ Test SEO

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import { persistor, store } from "@/store/redux";
+import { SEO } from "@/components/seo";
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -19,6 +20,10 @@ export default function App({ Component, pageProps }) {
       <PersistGate persistor={persistor}>
         <QueryClientProvider client={queryClient}>
           <div className={jetbrainsMono.className}>
+            <SEO
+              title="Lemon Code??"
+              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum."
+            />
             <Component {...pageProps} />
             <ToastContainer />
           </div>
